f1bf631a79ac2ab4d686e25d88c7669d87f2bda1
AlexaCai <131326250+AlexaCai@users.noreply.github.com>
2023-09-06 12:25:09 -0600

    Start screen styling updates and (see commit des.)
    Implementation of navigation between Start Screen and Chat Screen

--- components/Start.js
+++ components/Start.js
@@ -1,24 +1,59 @@
 import { useState } from 'react';
-import { StyleSheet, View, Text, Button, TextInput, TouchableOpacity } from 'react-native';
+import { StyleSheet, View, Text, TextInput, TouchableOpacity, ImageBackground } from 'react-native';
 
 const StartScreen = ({ navigation }) => {
+
     const [name, setName] = useState('');
+    const [selectedColor, setSelectedColor] = useState('');
+
+    const image = require('../img/BackgroundImage.png');
 
     return (
         <View style={styles.container}>
-            <Text>Hello Screen1!</Text>
-            <TextInput
-                style={styles.textInput}
-                value={name}
-                onChangeText={setName}
-                placeholder='Your name'
-            />
-            <TouchableOpacity
-                style={styles.startChattingButton}
-                //***Addition an object { name: name } as a second parameter representing the data to be used in the screen the app is transitioning to (than in screen 2, { name: name } is extracted using prop).
-                onPress={() => navigation.navigate('ChatScreen', { name: name })}>
-                <Text style={styles.buttonText}>Start chatting</Text>
-            </TouchableOpacity>
+            <ImageBackground source={image} resizeMode="cover" style={styles.image}>
+                <Text style={styles.appTitle}>Chat App</Text>
+                <View style={styles.interactionBox}>
+                    <TextInput
+                        style={styles.textInput}
+                        value={name}
+                        onChangeText={setName}
+                        placeholder='Your name'
+                    />
+                    <Text style={styles.chooseBackGroundColorText}>Choose your chat background color</Text>
+                    <View style={styles.chatBackgroundColorContainer}>
+                        <TouchableOpacity style={styles.chatBackgroundColorBlackButton}
+                            onPress={() => {
+                                setSelectedColor("#090C08");
+                            }}>
+                            <View style={styles.chatBackgroundColorButton}></View>
+                        </TouchableOpacity>
+                        <TouchableOpacity style={styles.chatBackgroundColorPurpleButton}
+                            onPress={() => {
+                                setSelectedColor("#474056");
+                            }}>
+                            <View style={styles.chatBackgroundColorButton}></View>
+                        </TouchableOpacity>
+                        <TouchableOpacity style={styles.chatBackgroundColorGrayButton}
+                            onPress={() => {
+                                setSelectedColor("#8A95A5");
+                            }}>
+                            <View style={styles.chatBackgroundColorButton}></View>
+                        </TouchableOpacity>
+                        <TouchableOpacity style={styles.chatBackgroundColorGreenButton}
+                            onPress={() => {
+                                setSelectedColor("#B9C6AE");
+                            }}>
+                            <View style={styles.chatBackgroundColorButton}></View>
+                        </TouchableOpacity>
+                    </View>
+                    <TouchableOpacity
+                        style={styles.startChattingButton}
+                        //***Addition an object { name: name } as a second parameter representing the data to be used in the screen the app is transitioning to (than in screen 2, { name: name } is extracted using prop).
+                        onPress={() => navigation.navigate('ChatScreen', { name: name, selectedColor: selectedColor })}>
+                        <Text style={styles.buttonText}>Start chatting</Text>
+                    </TouchableOpacity>
+                </View>
+            </ImageBackground>
         </View>
     );
 }
@@ -30,6 +65,26 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center'
     },
+    image: {
+        flex: 1,
+        width: '100%',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    appTitle: {
+        flex: 0.5,
+        fontSize: 45,
+        fontWeight: "600",
+        color: "#FFFFFF",
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    interactionBox: {
+        backgroundColor: "#FFFFFF",
+        width: '90%',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
     textInput: {
         width: "88%",
         fontSize: 16,
@@ -37,17 +92,68 @@ const styles = StyleSheet.create({
         color: "#757083",
         opacity: 0.5,
         padding: 15,
-        borderWidth: 1,
-        marginTop: 15,
-        marginBottom: 15
+        borderWidth: 2,
+        marginTop: '5%',
+        marginBottom: '5%',
+    },
+    chooseBackGroundColorText: {
+        textAlign: 'left',
+        alignSelf: 'flex-start',
+        marginLeft: '7%'
+    },
+    chatBackgroundColorContainer: {
+        flexDirection: 'row',
+        justifyContent: 'flex-start',
+        width: '88%',
+        paddingTop: '5%',
+        paddingBottom: '7%',
+    },
+    chatBackgroundColorBlackButton: {
+        width: 50,
+        height: 50,
+        backgroundColor: "#090C08",
+        opacity: 1,
+        borderRadius: 25,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginRight: '5%',
+    },
+    chatBackgroundColorPurpleButton: {
+        width: 50,
+        height: 50,
+        backgroundColor: "#474056",
+        opacity: 1,
+        borderRadius: 25,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginRight: '5%',
+    },
+    chatBackgroundColorGrayButton: {
+        width: 50,
+        height: 50,
+        backgroundColor: "#8A95A5",
+        opacity: 1,
+        borderRadius: 25,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginRight: '5%',
+    },
+    chatBackgroundColorGreenButton: {
+        width: 50,
+        height: 50,
+        backgroundColor: "#B9C6AE",
+        opacity: 1,
+        borderRadius: 25,
+        justifyContent: 'center',
+        alignItems: 'center',
     },
     startChattingButton: {
         width: "88%",
         backgroundColor: "#757083",
         padding: 15,
         borderWidth: 1,
-        marginTop: 15,
-        marginBottom: 15
+        marginTop: '2%',
+        marginBottom: '5%'
     },
     buttonText: {
         fontSize: 16,
