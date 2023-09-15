641c7ff69f630004ccfd4277830166c89db4769a
AlexaCai <131326250+AlexaCai@users.noreply.github.com>
2023-09-14 21:40:13 -0600

    Production comment updates

--- components/CustomActions.js
+++ components/CustomActions.js
@@ -1,6 +1,5 @@
 //***File used to code the (+) button on the left side of the text input field to allow user to do more than just sending messages (e.g.: sharing images or location).
 
-//***Import all necessary components.
 import { useEffect } from "react";
 import { StyleSheet, TouchableOpacity, View, Text, Alert } from "react-native";
 import { useActionSheet } from '@expo/react-native-action-sheet';
@@ -10,35 +9,26 @@ import * as ImagePicker from 'expo-image-picker';
 import * as Location from 'expo-location';
 
 
-//***Defines the component CustomActions. This component renders the what users see when they click on the (+) button in the text input field on the chat screen. 
 const CustomActions = ({ wrapperStyle, iconTextStyle, onSend, storage, userID }) => {
 
 
-    //***Used to initialize and create an actionSheet object by calling the useActionSheet hook provided by the @expo/react-native-action-sheet library (actionSheet object is used to display a menu with different action options when the user interacts with the (+) button in the chat interface).
+    //***Used to initialize and create an actionSheet object.
     const actionSheet = useActionSheet();
 
 
-    //***Variable that represents the reference to the recording object returned when calling Audio.Recording.createAsync() (variable is initialized with 'null').
     let recordingObject = null;
 
 
     //***Function used to display the different actions users can choose when they click on the (+) button on the left side of the text input bar.
     const onActionPress = () => {
-        //***Strings representing the different options that the user can choose from when they open the action sheet.
         const options = ['Select an image from library', 'Take a photo', 'Share location', 'Record a Sound', 'Cancel'];
-        //***The 'cancelButtonIndex' variable is used to specify which option within the options array above (here 'Cancel') should be treated as the cancel option when displaying the action sheet to the user. In this case, 'options.length' is equal to 4. - 1 subtracts 1 from the total number of options, thus adjusting the index to refer to the last element in the array ('Cancel' which is at position index 3 in the 'options array' - the last one).
         const cancelButtonIndex = options.length - 1;
-        //***Displays the action sheet with the specified options.
         actionSheet.showActionSheetWithOptions(
-            //***Display the available options and the cancel button in the user interface when the action sheet is shown.
             {
                 options,
                 cancelButtonIndex,
             },
-            //***Function executed when user selects an option from the action sheet. The buttonIndex parameter represents the index of the selected option below.
             async (buttonIndex) => {
-                //***'switch' is used to determine which option the user selected based on the buttonIndex. 
-                //***Depending on the selected option, one of three functions (pickImage, takePhoto, or getLocation) is called.
                 switch (buttonIndex) {
                     case 0:
                         pickImage();
@@ -62,46 +52,29 @@ const CustomActions = ({ wrapperStyle, iconTextStyle, onSend, storage, userID })
     //***Codes below related to phone images library and phone camera actions.
 
 
-    //***This function takes an image URI, uploads the image to Firebase Cloud Storage with a unique reference, retrieves the download URL, and sends it as a message in the chat UI.
     const uploadAndSendImage = async (imageURI) => {
-        //***Creation of a variable that takes the 'generateReference' function output below.
         const uniqueRefString = generateReference(imageURI);
-        //***ref() is a function imported from firebase/storage.'storage' passed to ref() is the Firebase Storage handler passed from Chat.js.
-        //***'uniqueRefString' as the second argument uses the output of the function 'generateReference' to associate a unique string/reference to every new image uploaded to the Cloud storage (when users send images in the chat screen).
         const newUploadRef = ref(storage, uniqueRefString);
-        //***Fetches the image data from the provided imageURI (downloads the image from the given URL).
         const response = await fetch(imageURI);
-        //***Converts the downloaded image data into a binary blob, which is a format suitable for uploading to Firebase Cloud Storage.
         const blob = await response.blob();
-        //***uploadBytes is the function that's used to upload the images users sent in Cloud storage, as well as rendering it as a message in the chat screen UI at the same time. When users pick an image from their phone's image library, not only it's uploaded to the Firebase Cloud Storage, but it's also sent as a message to the other receiving user can see it.
+        //***uploadBytes is the function that's used to upload the images users sent in Cloud storage, as well as rendering it as a message in the chat screen UI at the same time.
         uploadBytes(newUploadRef, blob).then(async (snapshot) => {
-            //***Get the remote URL of the image users just uploaded. First step to send the images from users as a message so that it gets rendered in a message bubble chat screen inside GiftedChat.
             const imageURL = await getDownloadURL(snapshot.ref)
-            //***Call the onSend() prop so the images are sent as a message in the chat UI screen whenever users sent them.
             onSend({ image: imageURL })
         });
     }
 
 
-    //***Functon allowing users to pick images from phone's library and send them.
     const pickImage = async () => {
-        //***This displays a prompt to the user UI asking for permission to access the media library.
         let permissions = await ImagePicker.requestMediaLibraryPermissionsAsync();
-        //***If users grant access to their device’s gallery, the function returns an object that has many properties - here this is referenced by the 'permissions' variable. To know if users have grant access, the property to check from the returned object is '.granted', which is a boolean (looking for TRUE).
         if (permissions?.granted) {
-            //***If users have granted access to their media library (permissions?.granted is TRUE), ImagePicker.launchImageLibraryAsync() is called to let them pick a file.
             let result = await ImagePicker.launchImageLibraryAsync();
-            //***ImagePicker.launchImageLibraryAsync() returns an object (referenced as const 'RESULT') containing the .assets property. This is an array referencing all of the users picked media files (referred to as 'assets'). Here, the array is limited to only having one asset [0] (users aren't allowed to pick multiple images).
-            //***The asset (image) contains a .uri - a string representing the path to the picked media file, along with its .width, .height, and file type.
-            //***The object 'RESULT' also contains the boolean property '.canceled'. This will be true if the user cancels the process and doesn’t pick a file/asset/image (which will close the selecting image screen and go back to the main chat UI screen).
             if (!result.canceled) await uploadAndSendImage(result.assets[0].uri);
             else Alert.alert("Permissions haven't been granted.");
         }
     }
 
 
-    //***Function allowing users to take photos from phone's camera and send them.
-    //***Working the same way as the pickImage function described above.
     const takePhoto = async () => {
         let permissions = await ImagePicker.requestCameraPermissionsAsync();
         if (permissions?.granted) {
@@ -113,7 +86,6 @@ const CustomActions = ({ wrapperStyle, iconTextStyle, onSend, storage, userID })
 
 
     //***Used to generate a unique reference string each time a new file/image is uploaded to the Cloud storage, to make it possible to upload multiple images. Without this function, everytime an image would be send by a user and upload to the Cloud database, it would replace the previous image uploaded (because of having the same reference string).
-    //***The function has one argument that represents the picked image’s URI. The function combines multiple strings to produce a string that can be used as a unique reference for the image to be uploaded.
     const generateReference = (uri) => {
         const timeStamp = (new Date()).getTime();
         const imageName = uri.split("/")[uri.split("/").length - 1];
@@ -125,18 +97,11 @@ const CustomActions = ({ wrapperStyle, iconTextStyle, onSend, storage, userID })
     //***Codes below related get the users phone location sharing.
 
 
-    //***Function used to retrieve the device's current location and send it as a message.
     const getLocation = async () => {
-        //***Prompt send to users UI to ask for location permission, and store the result in 'permissions' variable.
         let permissions = await Location.requestForegroundPermissionsAsync();
-        //***Check if location permission have been granted.
         if (permissions?.granted) {
-            //***If permission is granted, retrieve the current device location and store it in 'location'.
             const location = await Location.getCurrentPositionAsync({});
-            //***Check if a valid 'location' object was obtained.
             if (location) {
-                //***The location coordinates from CustomActions.js are passed to the Chat.js file through the 'onSend' prop. 
-                //***'onSend' sends a message that only contains the location property.
                 onSend({
                     //***The object assigned to the location property sent in 'onSend' contains all the data necessary for 'renderCustomView' function in Chat.js to render the MapView in a message bubble in the chat UI screen.
                     location: {
@@ -152,12 +117,9 @@ const CustomActions = ({ wrapperStyle, iconTextStyle, onSend, storage, userID })
     //***Codes below related to recording and playing sent / received audios.
 
 
-    //***startRecording() function handles starting the recording.
     const startRecording = async () => {
         try {
-            //***First asking permission to use the device’s microphone.
             let permissions = await Audio.requestPermissionsAsync();
-            //***If user give permission, code block below is triggered.
             if (permissions?.granted) {
                 //***iOS specific config to allow recording on iPhone devices.
                 await Audio.setAudioModeAsync({
@@ -170,11 +132,8 @@ const CustomActions = ({ wrapperStyle, iconTextStyle, onSend, storage, userID })
                         return results.recording;
                     }).then(recording => {
                         recordingObject = recording;
-                        //***whenever users press on the 'Record a Sound' action, they are allow to perform two actions at that point: cancel the recording or send it. 
                         Alert.alert('You are recording...', undefined, [
-                            //***When users press 'cancel' button, stopRecording function below is called (to stop the recording and unloads it from memory).
                             { text: 'Cancel', onPress: () => { stopRecording() } },
-                            //***When users press 'Stop and Send' button, sendRecordedSound function below is called (to store and send the audio).
                             {
                                 text: 'Stop and Send', onPress: () => {
                                     sendRecordedSound()
@@ -191,7 +150,6 @@ const CustomActions = ({ wrapperStyle, iconTextStyle, onSend, storage, userID })
     }
 
 
-    //***stopRecording() function stops the recording and unloads it from the memory.
     const stopRecording = async () => {
         await Audio.setAudioModeAsync({
             allowsRecordingIOS: false,
@@ -201,18 +159,15 @@ const CustomActions = ({ wrapperStyle, iconTextStyle, onSend, storage, userID })
     }
 
 
-    //***sendRecordedSound() function uses the 'stopRecording' function above stop the recording and then uploads the recorded sound file from the recording object (similar way to how images are uploaded above to the Firebase Storage) and sends it.
     const sendRecordedSound = async () => {
         await stopRecording()
         const uniqueRefString =
-            //***The sound file’s URI is extracted by calling the recording object’s getURI() method
             generateReference(recordingObject.getURI());
         const newUploadRef = ref(storage, uniqueRefString);
         const response = await fetch(recordingObject.getURI());
         const blob = await response.blob();
         uploadBytes(newUploadRef, blob).then(async (snapshot) => {
             const soundURL = await getDownloadURL(snapshot.ref)
-            //***When the message is sent using onSend(), the audio key is sets of the Gifted Chat message object.
             onSend({ audio: soundURL })
         });
     }
@@ -228,7 +183,6 @@ const CustomActions = ({ wrapperStyle, iconTextStyle, onSend, storage, userID })
 
 
     return (
-        //***When users click on the (+) button on the left side of the text input bar, 'onActionPress' function is called.
         <TouchableOpacity
             style={styles.container}
             accessible={true}
@@ -244,7 +198,6 @@ const CustomActions = ({ wrapperStyle, iconTextStyle, onSend, storage, userID })
 }
 
 
-//***Styling logics for all elements in this file.
 const styles = StyleSheet.create({
     container: {
         width: 26,
--- components/Start.js
+++ components/Start.js
@@ -1,14 +1,11 @@
 //***When the app is started, StartScreen appears to users (this file). When users press the 'Start chatting' button on StartScreen, the app transition to the ChatScreen. 
 
-//***Import all necessary components.
 import { useState } from 'react';
 import { StyleSheet, View, Text, TextInput, TouchableOpacity, ImageBackground, KeyboardAvoidingView, Platform, Alert } from 'react-native';
 import { getAuth, signInAnonymously } from "firebase/auth";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 
-
-//***Defines the component StartScreen. This component renders the user interface of the StartScreen. 
 const StartScreen = ({ navigation }) => {
 
 
@@ -16,29 +13,21 @@ const StartScreen = ({ navigation }) => {
     const auth = getAuth();
 
 
-    //***Used to get the name user types in the TextInput and display it on the header of the Chat Screen (via props extraction in Chat.js).
     const [name, setName] = useState('');
-    //***Used to get the background user choose and display it on the Chat Screen background (via props extraction in Chat.js).
     const [selectedColor, setSelectedColor] = useState('');
-    //***Used to requiere the background image for the Start Screen ('const image' is used below to display it).
     const image = require('../img/BackgroundImage.png');
 
-    //***Function call when user click 'Start chatting' button on the start screen. This 'const signInUser' allows the user to sign in anonymously.
+    //***Function call when user click 'Start chatting' button on the start screen.
     const signInUser = async () => {
         let userId = await AsyncStorage.getItem("userId");
 
-        //***If there's no user ID locally stored on the device (first time user), a new user ID is assigned to the user, and this user ID is stored locally on his device. The next time user will go on the app, this same user UI will be used to sign in.
+        //***If there's no user ID locally stored on the device (first time user), a new user ID is assigned to the user, and this user ID is stored locally on his device. 
         if (!userId) {
-            //*** If user ID not found locally, Firebase authentication is used to generate a new one.
             signInAnonymously(auth)
-                //***'result' is an information object regarding the temporary user account.
                 .then(result => {
-                    //***Assign the Firebase user ID to userId.
                     userId = result.user.uid;
-                    //***Store the new Firebase user ID locally for future sessions.
                     AsyncStorage.setItem("userId", userId)
                         .then(() => {
-                            //***After storing the user ID locally, navigate to the ChatScreen.
                             navigation.navigate("ChatScreen", {
                                 userID: userId,
                                 name: name,
@@ -54,9 +43,8 @@ const StartScreen = ({ navigation }) => {
                     console.error("Error signing in anonymously:", error);
                     Alert.alert("Unable to sign in, try again later.");
                 });
-            //***If user is not at his first visit on the app, he already have his user ID stored locally to sign in. This user ID is used.
+            //***If user is not at his first visit on the app, he already has his user ID stored locally to sign in. This user ID is used.
         } else {
-            //***Using the user ID stored locally, navigate to chat screen after user click on 'Start chatting' button.
             navigation.navigate("ChatScreen", {
                 userID: userId,
                 name: name,
@@ -66,50 +54,37 @@ const StartScreen = ({ navigation }) => {
     }
 
 
-
-    //***'return" defines the structure of the screen (title, input box, button, etc).
     return (
         <View style={styles.container}>
-            {/* Background image inserted here to display it on app's Start screen background (source={image} referring to the 'const image' defined above). */}
             <ImageBackground source={image} resizeMode="cover" style={styles.image}>
                 <Text style={styles.appTitle}>Chat App</Text>
-                {/* View added here to apply stylings to the interaction zone on app's Start Screen (TextInput, color choosing, start chatting button). */}
                 <View style={styles.interactionBox}>
-                    {/* Input bar for users to enter their name. */}
                     <TextInput
                         style={styles.textInput}
-                        //***{name} here refers to the ''const name, setName'' useState above. At first, this is empty like defined ('').
                         value={name}
-                        //***Ensure that when user types something, setName is used to update the ''name'' value in 'const name, setName' above.
                         onChangeText={setName}
                         placeholder='Your name'
                     />
                     <Text style={styles.chooseBackGroundColorText}>Choose your chat background color:</Text>
-                    {/* View added here to apply stylings to the line presenting the four different background color options. */}
                     <View style={styles.chatBackgroundColorContainer}>
-                        {/* Button to choose the black background color. */}
                         <TouchableOpacity style={styles.chatBackgroundColorBlackButton}
-                            //***When user clicks on this button (onPress), setSelectedColor is used to update the selectedColor value in 'const selectedColor, setSelectedColor' useState above (and this updated value is later used to set up the ChatScreen background color accordingly).
                             onPress={() => {
                                 setSelectedColor("#090C08");
                             }}>
                             <View style={styles.chatBackgroundColorButton}></View>
                         </TouchableOpacity>
-                        {/* Purple background button: same principle than for the black background color above. */}
                         <TouchableOpacity style={styles.chatBackgroundColorPurpleButton}
                             onPress={() => {
                                 setSelectedColor("#474056");
                             }}>
                             <View style={styles.chatBackgroundColorButton}></View>
                         </TouchableOpacity>
-                        {/* Gray background button: same principle than for the black background color above. */}
                         <TouchableOpacity style={styles.chatBackgroundColorGrayButton}
                             onPress={() => {
                                 setSelectedColor("#8A95A5");
                             }}>
                             <View style={styles.chatBackgroundColorButton}></View>
                         </TouchableOpacity>
-                        {/* Green background button: same principle than for black background color above. */}
                         <TouchableOpacity style={styles.chatBackgroundColorGreenButton}
                             onPress={() => {
                                 setSelectedColor("#B9C6AE");
@@ -117,10 +92,8 @@ const StartScreen = ({ navigation }) => {
                             <View style={styles.chatBackgroundColorButton}></View>
                         </TouchableOpacity>
                     </View>
-                    {/* Button for user to click on and be redirected to ChatScreen. */}
                     <TouchableOpacity
                         style={styles.startChattingButton}
-                        //***When user click on the button (onPress), signInUser is called (see above where the function is defined).
                         onPress={signInUser}>
                         <Text style={styles.buttonText}>Start chatting</Text>
                     </TouchableOpacity>
@@ -133,7 +106,6 @@ const StartScreen = ({ navigation }) => {
 }
 
 
-//***Styling logics for all elements in this file.
 const styles = StyleSheet.create({
     container: {
         flex: 1,
